5d2e1a0a9b84d2f8defd86d7f10ba937fbe8ef65
antikaka <keistakojine@gmail.com>
2024-04-21 17:00:47 +0300

    0.12v -Optimized a bit (removed repetitive list generation) -Improved colors, added black square outlines

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def left_right(t_rect1, t_rect2):
     if rect2_h < 10:
         solo_rect = True
 
-    x = t_rect1[0]  #x'as pagal ilgi irgi turi but
+    x = t_rect1[0]
     y = t_rect1[1] + 20
 
     for num in range(rect1_h // 20):
@@ -318,8 +318,6 @@ started = False
 start_pos = (380, 20)
 
 piece_list = []
-piece_dict = {}
-
 
 piece_x, piece_y = start_pos[0], start_pos[1]
 
@@ -342,6 +340,8 @@ new_piece = False
 
 speed = 3
 pg.key.set_repeat(500, 50)
+
+playfield_closed = []
 while True:
     clock.tick(speed)
 
@@ -366,29 +366,14 @@ while True:
     start_button = pg.Rect(0, 10, 20, 20)
     pg.draw.rect(screen, (255, 0, 0), start_button)
 
-    playfield_open = playfield_list.copy()
 
-    for sq in playfield_list:
-        for piece in piece_list:
-            if (sq.colliderect(piece[0]) or sq.colliderect(piece[1])) and sq in playfield_open:
-                playfield_open.remove(sq)
-
-    playfield_closed = []
-
-    for sq in playfield_list:
-        if sq in playfield_open:
-            continue
-        else:
-            playfield_closed.append(sq)
 
     playfield_active = []
-    for piece in piece_list:
+    for sq_c in playfield_closed:
         for sq in playfield_list:
-            if sq.colliderect(piece[0]) or sq.colliderect(piece[1]):
-                for sq2 in playfield_open:
-                    if sq[0] == sq2[0] and sq[1] - 20 == sq2[1]:
-                        if sq2 not in playfield_active:
-                            playfield_active.append(sq2)
+            if sq not in playfield_closed and sq_c[2][0] == sq[0] and sq_c[2][1] - 20 == sq[1]:
+                playfield_active.append(sq)
+
 
     playfield_active_bottoms = []
     for sq in playfield_active:
@@ -397,7 +382,7 @@ while True:
 
     go_right = True
     go_left = True
-    if len(piece_list) > 0:
+    if len(playfield_closed) > 1:
         obj_l, obj_r = left_right(t_rect1, t_rect2)
 
         for sq in obj_l:
@@ -407,15 +392,17 @@ while True:
 
 
         for sq in obj_l:
-            if sq in playfield_closed:
-                go_left = False
-                break
+            for sq_c in playfield_closed:
+                if sq == sq_c[2]:
+                    go_left = False
+                    break
 
 
         for sq in obj_r:
-            if sq in playfield_closed:
-                go_right = False
-                break
+            for sq_c in playfield_closed:
+                if sq == sq_c[2]:
+                    go_right = False
+                    break
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -446,14 +433,15 @@ while True:
     elif count < 1:
         count = 4
 
-    if piece_count != len(piece_list) and new_piece:
-        random_color = random.choice(colors)
 
-    if piece_count != len(piece_list) and new_piece:
-        print("piece_list+")
-        for piece in piece_dict.values():
-            if piece[0][2] not in piece_list:
-                piece_list.append((piece[0][2], piece[1][2]))
+    if new_piece:
+        for sq in playfield_list:
+            for piece in piece_list:
+                if sq.colliderect(piece[2]):
+                    playfield_closed.append((screen, piece[1], sq))
+        piece_list.clear()
+        print(piece_list)
+        random_color = random.choice(colors)
 
         new_piece = False
 
@@ -462,7 +450,8 @@ while True:
             print("collide")
             piece_count += 1
             new_piece = True
-            piece_dict[piece_count] = ((screen, random_color, t_rect1), (screen, random_color, t_rect2))
+            piece_list.append((screen, random_color, t_rect1))
+            piece_list.append((screen, random_color, t_rect2))
             random_shape = random.choice(shape_list)
             piece_x, piece_y = start_pos[0], start_pos[1]
             t_rect1, t_rect2, x_fix = random_shape(count, piece_x, piece_y)
@@ -476,7 +465,8 @@ while True:
             print("collide2")
             piece_count += 1
             new_piece = True
-            piece_dict[piece_count] = ((screen, random_color, t_rect1), (screen, random_color, t_rect2))
+            piece_list.append((screen, random_color, t_rect1))
+            piece_list.append((screen, random_color, t_rect2))
             random_shape = random.choice(shape_list)
             piece_x, piece_y = start_pos[0], start_pos[1]
             t_rect1, t_rect2, x_fix = random_shape(count, piece_x, piece_y)
@@ -489,15 +479,13 @@ while True:
     if t_rect2[2] > 1:
         pg.draw.rect(screen, random_color, t_rect2)
 
-    for piece in piece_dict.values():
-        pg.draw.rect(*piece[0])
-        pg.draw.rect(*piece[1])
-
-    for sq in playfield_active:
-        pg.draw.rect(screen, (255, 0, 0), sq, 1)
+    for sq in playfield_closed:
+        pg.draw.rect(*sq)
+        pg.draw.rect(screen, (0, 0, 0), sq[2], 1)
 
     piece_x = t_rect1[0] + x_fix
     piece_x, piece_y = drop(piece_x, piece_y)
+
 
     pg.display.flip()
 
